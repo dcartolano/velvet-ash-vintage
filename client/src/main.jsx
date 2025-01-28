e46ead9a49b24b342b9da05248d293b2e1e71833
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-01-28 16:44:57 -0600

    changed from boiler plate to format for react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App';
+import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
+import AboutPage from './pages/AboutPage';
+import GalleryPage from './pages/GalleryPage';
+import ShopPage from './pages/ShopPage';
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: '/about',
+                element: <AboutPage />,
+            },
+            {
+                path: '/gallery',
+                element: <GalleryPage />,
+            },
+            {
+                path: '/shop',
+                element: <ShopPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
