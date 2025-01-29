f079a02d4160ec0395559e99e96b6f30e3a6bad8
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-01-29 15:26:31 -0600

    reordered to place app.use(routes) lower, moved general app.get statement outside of possible prodcution if statement

--- server/server.js
+++ server/server.js
@@ -19,11 +19,14 @@ app.use(routes);
 // if we're in production, serve client/build as static assets
 // if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
+// }
 
-  app.get('*', (_req, res) => {
+  app.use(routes);
+
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
-// }
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
