61e3768b7a6555d5551aa8d2aff5b1a0f2f5b784
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-01-29 15:37:46 -0600

    restructured to add in database info but left commented out for later, commented out /api route test, removed duplicate app.use(routes) statement, enabled and expanded for loop for production, added comments explaining some parts

--- server/server.js
+++ server/server.js
@@ -1,33 +1,41 @@
 import express from 'express';
+// import db from './config/connection.js';
 import path from 'node:path'
+import dotenv from 'dotenv';
+import path from 'path';
 import routes from './routes/index.js'
 
 const __dirname = import.meta.dirname;
 
+// await db();
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/api', (req, res) => {
-  res.send('Hello from the Node.js backend api!');
-});
+// app.get('/api', (req, res) => {
+//   res.send('Hello from the Node.js backend api!');
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+} else {
+  app.use(express.static('../client/dist'));
+}
+
 app.use(routes);
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-// }
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
-  app.use(routes);
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
