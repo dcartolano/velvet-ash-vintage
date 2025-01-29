81fe931f8f1f7fc8c2028f5a5cb199cd545d88a9
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-01-29 11:25:58 -0600

    added and made simple customizations to bootstrap templates

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,7 +1,42 @@
+import logo from '../../assets/vav-logo.png';
+
 const Navigation = () => {
     return (
         <div>
-            <h3>Nav</h3>
+            {/* <div class="pos-f-t"> */}
+
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    <img src={logo} width="50" height="50" className="d-inline-block align-center" alt=""></img>
+                        Velvet Ash Vintage
+                </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    {/* <div className="bg-light p-4"> */}
+                        {/* <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/gallery">Gallery</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/shop">Shop</a>
+                            </li>
+                        </ul> */}
+                        <a className="nav-item nav-link" href="/">Home</a>
+                        <a className="nav-item nav-link" href="/about">About</a>
+                        <a className="nav-item nav-link" href="/gallery">Gallery</a>
+                        <a className="nav-item nav-link" href="/shop">Shop</a>
+                    {/* </div> */}
+                </div>
+            </nav>
+            {/* </div> */}
         </div>
     );
 }
