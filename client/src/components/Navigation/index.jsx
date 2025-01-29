74b0f363a9f17c3694c618909328526466acf3fe
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-01-29 11:28:13 -0600

    removed unused code

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -3,8 +3,6 @@ import logo from '../../assets/vav-logo.png';
 const Navigation = () => {
     return (
         <div>
-            {/* <div class="pos-f-t"> */}
-
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="/">
                     <img src={logo} width="50" height="50" className="d-inline-block align-center" alt=""></img>
@@ -14,29 +12,12 @@ const Navigation = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    {/* <div className="bg-light p-4"> */}
-                        {/* <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/gallery">Gallery</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/shop">Shop</a>
-                            </li>
-                        </ul> */}
                         <a className="nav-item nav-link" href="/">Home</a>
                         <a className="nav-item nav-link" href="/about">About</a>
                         <a className="nav-item nav-link" href="/gallery">Gallery</a>
                         <a className="nav-item nav-link" href="/shop">Shop</a>
-                    {/* </div> */}
                 </div>
             </nav>
-            {/* </div> */}
         </div>
     );
 }
