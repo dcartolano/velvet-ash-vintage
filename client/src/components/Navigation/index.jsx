f36d384764766891e047c9700c1bc1a769e4dfe1
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-03-03 18:26:23 -0600

    removed background color, added margins to some parts, centered text vertically and horizontally,

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,13 +1,15 @@
 import vavLogo from '../../assets/vav-logo.png';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 
 const Navigation = () => {
     return (
         <div>
             {/* <nav className="navbar navbar-expand navbar-light bg-light"> */}
-            <nav className="navbar navbar-light bg-light">
+            <nav className="navbar navbar-light">
+                <Navbar></Navbar>
                 {/* page title (brand) with logo and name */}
-                <a className="navbar-brand" href="/">
-                    <img src={vavLogo} width="50" height="50" className="d-inline-block align-center" alt="Velvet Ash Vintage Logo"/>
+                <a className="navbar-brand m-3 h1" href="/">
+                    <img src={vavLogo} width="75" height="75" className="d-inline-block align-center mr-2" alt="Velvet Ash Vintage Logo"/>
                     Velvet Ash Vintage
                 </a>
                 {/* collapseable option for list of pages */}
@@ -23,7 +25,7 @@ const Navigation = () => {
                 {/* dropdown option for list of pages */}
                 <div className="nav-item dropdown align-center">
                     {/* option for 3 bar with dropdown arrow */}
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a className="nav-link dropdown-toggle mr-2" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     {/* option for 3 bar with collapse-style button */}
                     {/* <a className="navbar-toggler" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> */}
                     <span className="navbar-toggler-icon align-center"></span>
