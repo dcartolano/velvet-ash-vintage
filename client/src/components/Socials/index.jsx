e805a2fd70ca02ed9ffdd1e2325d2f5ec8dfddf1
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-03-04 19:24:34 -0600

    added title for socials and added in socials image imports

--- client/src/components/Socials/index.jsx
+++ client/src/components/Socials/index.jsx
@@ -1,11 +1,19 @@
 import { Container } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
+import etsyLogo from '../../assets/etsy-logo.png';
+import instagramIcon from '../../assets/instagram-icon-2.png';
+import instagramLogoBlue from '../../assets/instagram-logo-blue.svg';
+// import instagramLogoBlack from '../../assets/instagram-logo-black.png';
+import depopLogo from '../../assets/depop-logo.svg';
+import tiktokLogo from '../../assets/tiktok-logo.png';
+import linktreeLogo from '../../assets/linktree-logo.png';
+
 const Socials = () => {
 
     return (
         <Container>
-
+            <h3>Socials</h3>
         </Container>
     );
 }
