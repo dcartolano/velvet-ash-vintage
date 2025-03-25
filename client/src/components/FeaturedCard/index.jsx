9ebbaa3989f6fb89eaba1799cc4d0c7cc9cf06d3
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-03-25 15:06:34 -0500

    converted html formatting over to react-bootstrap standards

--- client/src/components/FeaturedCard/index.jsx
+++ client/src/components/FeaturedCard/index.jsx
@@ -1,27 +1,24 @@
 import './index.css';
 
-import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
 
 const FeaturedCard = (product) => {
-
     // console.log(product);
     const currentProduct = product.product;
+
     return (
-        // <a href={currentProduct.productUrl}>
-        // <div className="card" style={{width: 18 + 'em'}}>
-        <div className="card border-0">
-            <a href={currentProduct.productUrl} target="_blank">
-                <Image className="card-img-top" src={currentProduct.productImage} alt={currentProduct.productTitle} rounded fluid/>
-                <div className="card-body">
-                    <p className="card-title fs-6 fw-medium">{currentProduct.productTitle}</p>
-                    <p className="card-text fw-light">{currentProduct.productPrice}</p>
-                    {/* <a href={currentProduct.productUrl} className="btn btn-primary">Go somewhere</a> */}
-                </div>
-            </a>
-        </div>
-        // </a>
+        <Card.Link href={currentProduct.productUrl} className="text-decoration-none text-start" target="_blank">
+            <Card className="border-0" text="dark" >
+                <Card.Body>
+                    <Card.Img className="rounded-0" variant="top" src={currentProduct.productImage} alt={currentProduct.productTitle} />
+                    <Card.Body>
+                        <Card.Title className="fs-6">{currentProduct.productTitle}</Card.Title>
+                        <Card.Text className="fw-light">{currentProduct.productPrice}</Card.Text>
+                    </Card.Body>
+                </Card.Body>
+            </Card>
+        </Card.Link>
     );
 }
 
 export default FeaturedCard;
-
