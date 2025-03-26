153188358be95205ed61b91035aac52bba040d14
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-03-25 20:14:21 -0500

    deleted all imports that were no longer being used; renamed original image import and added additional option that was cropped to remove excess spacing above subject; made page title bold and added class; removed class from second row as no longer needed; removed height from column properties as that label doesn't do anything there; changed image used to newer cropped photo; adjusted line spacing for bio and added class

--- client/src/components/AboutSection/index.jsx
+++ client/src/components/AboutSection/index.jsx
@@ -1,33 +1,28 @@
 import './index.css';
 
-import ashPhoto from '../../assets/ash-about-1.jpg';
+// import ashPhoto1 from '../../assets/ash-about-1.jpg';
+import ashPhoto2 from '../../assets/ash-about-2.jpg';
 
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ThemeProvider from 'react-bootstrap/ThemeProvider'
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-// import { Stack } from 'react-bootstrap';
-import Stack from 'react-bootstrap/Stack';
-import { Link } from 'react-router-dom';
-import Ratio from 'react-bootstrap/Ratio';
-
 
 const AboutSection = () => {
+    let breakpoint;
+
     return (
         <Container fluid>
             <Row>
                 <Col>
-                    <p className="fs-2">WELCOME TO VELVET ASH VINTAGE</p>
+                    <p className="fs-2 fw-bold about-title">WELCOME TO VELVET ASH VINTAGE</p>
                 </Col>
             </Row>
-            <Row className="justify-content-md-center image-row">
-                <Col className="image-column" xs={{ span: 8, offset: 2, height: 2 }} md={{ span: 4, offset: 0 }}>
-                    <Image className="about-image" src={ashPhoto} fluid />
+            <Row className="justify-content-md-center">
+                <Col className="image-column" xs={{ span: 8, offset: 2 }} md={{ span: 4, offset: 0 }}>
+                    <Image className="about-image" src={ashPhoto2} fluid />
                 </Col>
-                <Col className='text-start' xs={{ span: 10, offset: 1 }} md={{ span: 4, offset: 0 }}>
+                <Col className='text-start lh-lg about-bio' xs={{ span: 10, offset: 1 }} md={{ span: 4, offset: 0 }}>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi illum autem soluta, est ab ad recusandae delectus eos ipsa, assumenda ipsum deserunt esse! Quaerat aspernatur hic possimus rerum esse sed impedit quis eligendi repellat mollitia.</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima accusamus voluptates porro ipsum, nobis fuga impedit veritatis eligendi eveniet necessitatibus excepturi tenetur, quo similique dicta.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corrupti, libero obcaecati possimus sit maiores, rerum nesciunt itaque eaque magni deserunt iure, incidunt esse quidem. Voluptatem labore ratione adipisci facilis.</p>
