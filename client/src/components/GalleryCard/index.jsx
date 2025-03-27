37def03109a3c314fb684bc1a7e0a73dd335045b
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-03-27 17:27:56 -0500

    imported css file; imported react-bootstrap and react-router-dom components; pulled in more data from Tumblr API, but left some commented out as not currently being used; added string methods to pull source image url out of full html text from Tumblr; redid hmtl content for new data since old method no longer needed

--- client/src/components/GalleryCard/index.jsx
+++ client/src/components/GalleryCard/index.jsx
@@ -1,25 +1,57 @@
+import './index.css';
+
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+
 const GalleryCard = (post) => {
 
     // console.log('post: ', post);
-    const parentPostUrl = post.post.parentPostUrl;
 
-    // console.log('parentPostUrl: ', parentPostUrl);
-    const postContent = post.post.postContent;
-
-    // console.log('postContent: ', postContent);
-    const postId = post.post.postId;
-    
+    // const postId = post.post.postId;
     // console.log('postId: ', postId);
+    const postUrl = post.post.postUrl;
+    // console.log('postUrl: ', postUrl);
+    const postSummary = post.post.postSummary;
+    // console.log('postSummary: ', postSummary);
+    const parentPoster = post.post.parentPoster;
+    // console.log('parentPoster: ', parentPoster);
+    // const parentPostUrl = post.post.parentPostUrl;
+    // console.log('parentPostUrl: ', parentPostUrl);
+    // const parentPostId = post.post.parentPostId;
+    // console.log('parentPostId: ', parentPostId);
+    const postContent = post.post.postContent;
+    // console.log('postContent: ', postContent);
 
     const decodedPostContent = decodeURIComponent(postContent);
     // console.log('decodedPostContent: ', decodedPostContent);
+    const decodedPostContentSplit = decodedPostContent.split(" ");
+    // console.log(decodedPostContentSplit[2]);
+    const decodedPostContentSplitReplaced = decodedPostContentSplit[2].replace("src=", "");
+    // console.log(decodedPostContentSplitReplaced);
+    const decodedPostContentSplitReplacedSliced = decodedPostContentSplitReplaced.slice(1, -1);
+    // console.log(decodedPostContentSplitReplacedSliced);
+
+    const videoBoolean = /video/.test(decodedPostContentSplitReplacedSliced);
+    // console.log(videoBoolean);
+
 
     return (
         <div>
-            <div className="" fluid>
+            {/* <div className="">
                 <div className="" dangerouslySetInnerHTML={{ __html: decodedPostContent }}></div>
-            </div>
-        </div>
+            </div> */}
+            {videoBoolean ? (
+                <div></div>
+            ) : (
+                <Container text="dark">
+                    <Link to={postUrl} className="text-decoration-none" target="_blank">
+                        <Image src={decodedPostContentSplitReplacedSliced} fluid/>
+                        <p>{postSummary} via {parentPoster} on Tumblr</p>
+                    </Link>
+                </Container>
+            )}
+        </div >
     );
 }
 
