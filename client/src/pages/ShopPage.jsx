b92724bbfde6ad7da1377f798b5b955faa26c4f7
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-07 10:24:36 -0500

    removed outdated info from shop page and cleaned up

--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -1,41 +1,9 @@
-// import etsyLogo from '../assets/etsy-logo.png';
-// import instagramIcon from '../assets/instagram-icon-2.png';
-// import instagramLogoBlue from '../assets/instagram-logo-blue.svg';
-// import instagramLogoBlack from '../assets/instagram-logo-black.png';
-// import depopLogo from '../assets/depop-logo.svg';
-// import tiktokLogo from '../assets/tiktok-logo.png';
-// import linktreeLogo from '../assets/linktree-logo.png';
+
 
 const ShopPage = () => {
     return (
         <div className="p-3">
-            {/* <h3>Shop</h3>
-            <div className="p-3">
-                <a href="https://www.etsy.com/shop/velvetashvintage">
-                    <img src={etsyLogo} height="50" alt="Etsy Logo" />
-                </a>
-            </div>
-            <div className="p-3">
-                <a href="https://www.instagram.com/velvetashvintage/">
-                    <img src={instagramIcon} height="50" className="mr-1" alt="Instagram Icon" />
-                    <img src={instagramLogoBlue} height="50" alt="Instagram Logo" />
-                </a>
-            </div>
-            <div className="p-3">
-                <a href="https://www.depop.com/velvetashvintage/">
-                    <img src={depopLogo} height="50" alt="Depop Logo" />
-                </a>
-            </div>
-            <div className="p-3">
-                <a href="https://linktr.ee/velvetashvintage">
-                    <img src={linktreeLogo} height="50" alt="Linktree Logo" />
-                </a>
-            </div>
-            <div className="p-3">
-                <a href="https://www.tiktok.com/@velvetashvintage">
-                    <img src={tiktokLogo} height="50" alt="TikTok Logo" />
-                </a>
-            </div> */}
+            <h3>Shop</h3>
         </div>
     );
 }
