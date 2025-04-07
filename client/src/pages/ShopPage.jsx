584f7513423bbe737a7d776447b49397aabeb830
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-07 14:17:13 -0500

    added useState and useEffect for page to pull etsy information from route once setup; adjusted name of variables in existing code to match new additions; reworded message for case when no featured items are pulled in

--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 import FeaturedCard from '../components/FeaturedCard';
 
 import Container from 'react-bootstrap/Container';
@@ -5,14 +7,38 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const ShopPage = () => {
+
+const [shopData, setShopData] = useState([]);
+
+    const fetchShopData = async () => {
+        try {
+            const response = await fetch('/api/getFeaturedItems');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data);
+
+            setShopData(data);
+
+        } catch (error) {
+            console.error('Failed to fetch shop listings:', error);
+            throw error;
+        }
+    };
+
+    useEffect(() => {
+        fetchShopData();
+    }, []);
+
     return (
         <div className="">
             <Container className="mb-5" fluid>
                 <p className="fs-3 fw-light">Featured Items</p>
                 <Row xs={2} sm={3} md={3} lg={4}>
                     {/* <Row> */}
-                    {featuredItems.length > 0 ? (
-                        featuredItems.map((product, index) => (
+                    {shopData.length > 0 ? (
+                        shopData.map((product, index) => (
                             <Col xs={6} md={4} lg={3} className="p-0">
                                 {/* <Col> */}
                                 <FeaturedCard
@@ -23,7 +49,7 @@ const ShopPage = () => {
                         ))
                     ) : (
                         <div>
-                            <p>please add a featured item!</p>
+                            <p>no featured items to show!</p>
                         </div>
                     )}
                 </Row>
