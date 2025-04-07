a959d5d8cc3eb70e9efeb069824a2420b2cd396b
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-07 10:33:23 -0500

    added in featured card format and required imports copied from homepage to set up for integrating etsy api

--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -1,10 +1,34 @@
+import FeaturedCard from '../components/FeaturedCard';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const ShopPage = () => {
     return (
-        <div className="p-3">
-            <h3>Shop</h3>
-        </div>
+        <div className="">
+            <Container className="mb-5" fluid>
+                <p className="fs-3 fw-light">Featured Items</p>
+                <Row xs={2} sm={3} md={3} lg={4}>
+                    {/* <Row> */}
+                    {featuredItems.length > 0 ? (
+                        featuredItems.map((product, index) => (
+                            <Col xs={6} md={4} lg={3} className="p-0">
+                                {/* <Col> */}
+                                <FeaturedCard
+                                    product={product}
+                                    key={index}
+                                />
+                            </Col>
+                        ))
+                    ) : (
+                        <div>
+                            <p>please add a featured item!</p>
+                        </div>
+                    )}
+                </Row>
+            </Container>
+        </div >
     );
 }
 
