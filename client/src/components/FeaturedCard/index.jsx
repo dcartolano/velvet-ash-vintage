f60b67f59eb046fa286f16da0ab57020a9070ec8
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-08 11:08:41 -0500

    added link and proxy url for alt image; replaced contaiiner around card img with a div and added an alt card img

--- client/src/components/FeaturedCard/index.jsx
+++ client/src/components/FeaturedCard/index.jsx
@@ -11,14 +11,17 @@ const FeaturedCard = (product) => {
     const proxy = 'https://corsproxy.io/?url=';
     const imageUrl = currentProduct.listingImages[0];
     const proxiedImageUrl = proxy + imageUrl;
+    const altImageUrl = currentProduct.listingImages[1];
+    const proxiedAltImageUrl = proxy + altImageUrl;
 
     return (
         <Card.Link href={currentProduct.listingUrl} className="text-decoration-none text-start" target="_blank">
             <Card className="border-0" text="dark" >
                 <Card.Body>
-                    {/* <Container className="image-container"> */}
+                    <div className="image-container">
                         <Card.Img className="rounded-0 item-image" variant="top" src={proxiedImageUrl} alt={currentProduct.listingTitle} />
-                    {/* </Container> */}
+                        <Card.Img className="rounded-0 alt-image" variant="top" src={proxiedAltImageUrl} alt={currentProduct.listingTitle} />
+                    </div>
                     <Card.Body className="p-0 mt-2 item-info">
                         <Card.Title className="item-info m-0 fw-bold">{currentProduct.listingTitle}</Card.Title>
                         <Card.Text className="item-price fw-bold">{`$${currentProduct.listingPrice}`}</Card.Text>
