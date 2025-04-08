ece0bfc8bacdda3ee79d6e53c04d1f75df4e274b
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-07 19:17:45 -0500

    added variable for etsy listing limit; added url for active etsy listings api call; added route for active esty listings

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -9,11 +9,13 @@ const tumblrConsumerKey = process.env.TUMBLR_CONSUMER_KEY;
 const etsyKeystring = process.env.ETSY_KEYSTRING;
 
 const tumblrPostLimit = '10'; // str
+const etsyListingLimit = '20';
 // let etsyListingId = 0; // num
 // let listingImagesArray = []; // array
 
 const tumblrPostsUrl = `http://api.tumblr.com/v2/blog/caffeinatedaze.tumblr.com/posts?api_key=${tumblrConsumerKey}&limit=${tumblrPostLimit}`;
 const etsyFeaturedListingsUrl = `https://openapi.etsy.com/v3/application/shops/37691936/listings/featured`;
+const etsyActiveListingsUrl = `https://openapi.etsy.com/v3/application/shops/37691936/listings/active?limit=${etsyListingLimit}`;
 // const etsyListingImagesUrl = `https://openapi.etsy.com/v3/application/listings/${etsyListingId}/images`;
 
 router.route('/getGalleryPosts').get(async (_req, res) => {
@@ -74,6 +76,88 @@ router.route('/getFeaturedItems').get(async (_req, res) => {
 
     try {
         const etsyListingsResponse = await fetch(etsyFeaturedListingsUrl, {
+            method: 'GET',
+            headers: {
+                'x-api-key': etsyKeystring,
+            },
+        });
+
+        if (!etsyListingsResponse.ok) {
+            const errorBody = await etsyListingsResponse.text(); // Get the response as text
+            console.error('Error communicating with Etsy:', errorBody);
+            return res.send('Error fetching shop info: ' + errorBody);
+        }
+
+        const rawEtsyListings = await etsyListingsResponse.json();
+        // console.log('Etsy Listings Response:', rawEtsyListings); // Log the featured listings response for debugging
+
+        const etsyFeaturedListingsArray = rawEtsyListings.results;
+
+        const shopDataArray = await Promise.all(etsyFeaturedListingsArray.map(async (listing) => {
+
+            // const etsyListingId = listing.listing_id; // num
+
+
+            const etsyImagesResponse = await fetch(`https://openapi.etsy.com/v3/application/listings/${listing.listing_id}/images`, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'x-api-key': etsyKeystring,
+                },
+            });
+
+            if (!etsyImagesResponse.ok) {
+                const errorBody = await etsyImagesResponse.text(); // Get the response as text
+                console.error('Error communicating with Etsy:', errorBody);
+                return res.send('Error fetching shop info: ' + errorBody);
+            }
+
+            const rawEtsyImages = await etsyImagesResponse.json();
+            // console.log('Etsy Images Response:', rawEtsyImages); // Log the featured listings response for debugging
+
+            const etsyImagesArray = rawEtsyImages.results;
+
+            const listingImagesArray = await Promise.all(etsyImagesArray.map(async (image) => {
+                // return {
+                //     // listingId: listing.listing_id, // num (may not be needed)
+                //     listingTitle: listing.title, // str
+                //     // listingState: listing.state, // str (may not be needed)
+                //     listingUrl: listing.url, //str
+                //     // listingTags: listing.tags, // [str] (array of str) (may not be needed)
+                //     listingPrice: (listing.price.amount / listing.price.divisor) // num (divides price amount by price divisor)
+                // }
+                // console.log(image.url_570xN);
+                return image.url_570xN;
+            }));
+
+            console.log(listingImagesArray);
+
+            return {
+
+                listingImages: listingImagesArray,
+                // listingId: listing.listing_id, // num (may not be needed)
+                listingTitle: listing.title, // str
+                // listingState: listing.state, // str (may not be needed)
+                listingUrl: listing.url, //str
+                // listingTags: listing.tags, // [str] (array of str) (may not be needed)
+                listingPrice: (listing.price.amount / listing.price.divisor).toFixed(2) // num (divides price amount by price divisor)
+            }
+        }));
+
+        // console.log('shopDataArray: ', shopDataArray);
+
+        res.json(shopDataArray);
+
+    } catch (error) {
+        console.error('Error fetching featured listings:', error);
+        res.json('Error fetching shop content');
+    }
+});
+
+router.route('/getActiveItems').get(async (_req, res) => {
+
+    try {
+        const etsyListingsResponse = await fetch(etsyActiveListingsUrl, {
             method: 'GET',
             headers: {
                 'x-api-key': etsyKeystring,
