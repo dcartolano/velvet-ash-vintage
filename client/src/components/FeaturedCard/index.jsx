e64c4aa14daa0d9dedc5cfa65d6699afbc8ddcc2
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-08 08:30:49 -0500

    commented out unused image import; removed fluid from image and surrounding container to avoid console error after veryifying it is not needed here for desired behavior

--- client/src/components/FeaturedCard/index.jsx
+++ client/src/components/FeaturedCard/index.jsx
@@ -1,7 +1,7 @@
 import './index.css';
 
 import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image';
+// import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/esm/Container';
 
 const FeaturedCard = (product) => {
@@ -16,8 +16,8 @@ const FeaturedCard = (product) => {
         <Card.Link href={currentProduct.listingUrl} className="text-decoration-none text-start" target="_blank">
             <Card className="border-0" text="dark" >
                 <Card.Body>
-                    <Container className="image-container" fluid>
-                        <Card.Img className="rounded-0 item-image" variant="top" src={proxiedImageUrl} alt={currentProduct.listingTitle} fluid />
+                    <Container className="image-container">
+                        <Card.Img className="rounded-0 item-image" variant="top" src={proxiedImageUrl} alt={currentProduct.listingTitle} />
                     </Container>
                     <Card.Body className="p-0 mt-2 item-info">
                         <Card.Title className="item-info m-0">{currentProduct.listingTitle}</Card.Title>
