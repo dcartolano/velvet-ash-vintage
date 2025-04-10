127c16a8064e015e7b5cde23550c555b8e2af68f
dcartolano <180475468+dcartolano@users.noreply.github.com>
2025-04-10 10:23:51 -0500

    commented out unused import; removed proxy since moved to api file and adjusted associated references

--- client/src/components/FeaturedCard/index.jsx
+++ client/src/components/FeaturedCard/index.jsx
@@ -2,25 +2,20 @@ import './index.css';
 
 import Card from 'react-bootstrap/Card';
 // import Image from 'react-bootstrap/Image';
-import Container from 'react-bootstrap/esm/Container';
+// import Container from 'react-bootstrap/esm/Container';
 
 const FeaturedCard = (product) => {
     // console.log(product);
     const currentProduct = product.product;
-
-    const proxy = 'https://corsproxy.io/?url=';
-    const imageUrl = currentProduct.listingImages[0];
-    const proxiedImageUrl = proxy + imageUrl;
-    const altImageUrl = currentProduct.listingImages[1];
-    const proxiedAltImageUrl = proxy + altImageUrl;
 
     return (
         <Card.Link href={currentProduct.listingUrl} className="text-decoration-none text-start" target="_blank">
             <Card className="border-0" text="dark" >
                 <Card.Body>
                     <div className="image-container">
-                        <Card.Img className="rounded-0 item-image" variant="top" src={proxiedImageUrl} alt={currentProduct.listingTitle} />
-                        <Card.Img className="rounded-0 alt-image" variant="top" src={proxiedAltImageUrl} alt={currentProduct.listingTitle} />
+                        <Card.Img className="rounded-0 item-image" variant="top" src={currentProduct.listingImages[0]} alt={currentProduct.listingTitle} />
+                        <Card.Img className="rounded-0 alt-image" variant="top" src={currentProduct.listingImages[1]} alt={currentProduct.listingTitle} />
+
                     </div>
                     <Card.Body className="p-0 mt-2 item-info">
                         <Card.Title className="item-info m-0 fw-bold">{currentProduct.listingTitle}</Card.Title>
